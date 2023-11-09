cd250c12784d76af0e21b793ef2b73459f0c1d46
med3 <med.taha.inpt@gmail.com>
2023-11-09 09:57:09 +0000

    fix dag

--- dags/predict.py
+++ dags/predict.py
@@ -7,13 +7,21 @@ from airflow.decorators import dag, task
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-from dags.config import GENERATED_DATA_PATH, DATA_FOLDER
+from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, FEATURES_PATH, MODEL_PATH, PREDICTIONS_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
+from formation_indus_ds_avancee.train_and_predict import predict_with_io
+# from formation_indus_ds_avancee.data_loading import get_data_from_csv
 
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
      start_date=pendulum.today('UTC').add(hours=-1))
 def predict():
+
+    # @task
+    # def get_data_from_csv_task():
+    #     get_data_from_csv(train_data_path=TRAIN_DATA_PATH, data_folder=GENERATED_DATA_FOLDER)
+
+
     @task
     def prepare_features_with_io_task():
         features_path = os.path.join(DATA_FOLDER, f'prepared_features_{datetime.now()}.parquet')
@@ -22,12 +30,14 @@ def predict():
                                  training_mode=False)
         return features_path
 
-    # Start completing predict task
-    # predict = PythonOperator()
-    # End completing predict task
+    @task
+    def predict_with_io_task(feature_path: str) -> None:
+        predict_with_io(features_path=feature_path,
+            model_path= MODEL_PATH,
+            predictions_folder= PREDICTIONS_FOLDER)
 
-    # feature_path = prepare_features_with_io_task()
-    # predict_with_io_task(feature_path=feature_path)
+    features_path = prepare_features_with_io_task()
+    predict_with_io_task(features_path)
 
 
 predict_dag = predict()
